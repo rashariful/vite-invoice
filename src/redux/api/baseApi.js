9d1fa6f0f03bfcbec9e54426cf6a74eac9c86bd9
Source-Code-007 <euhansarkar@gmail.com>
2024-07-03 13:28:55 +0600

    Added: CI/CD Configs.

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://invapi.icchaporon.com/api/v1",
+    baseUrl: "https://invapi.icchaporon.com/api/v1",
   }),
   endpoints: () => ({}),
   tagTypes: ["invoice"],
