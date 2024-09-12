02e2a4f7d10f957144dfc4fb9604104bae451df8
euhansarkar <euhansarkar@gmail.com>
2024-09-12 19:01:19 +0600

    Updated: Production Backend API Route.

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://invapi.icchaporon.com/api/v1",
+    baseUrl: "https://invoice-server.icchaporon.com/api/v1",
     // baseUrl: "http://localhost:4000/api/v1",
   }),
   endpoints: () => ({}),
